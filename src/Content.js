2350ecea5c411653e202aa62be069b320cd9c6da
Karthik <getkar128@gmail.com>
2022-09-04 14:33:53 +0300

    useState hook

--- src/Content.js
+++ src/Content.js
@@ -2,13 +2,16 @@ import React, {useState} from "react"
 
 const Content = () => {
     const [name, setName] = useState('Karthik')
+    const [count, setCount] = useState(0);
     const handlenamechange = ()=> {
         const name = ["Bob", 'Kevin', "Karthik"];
         const int = Math.floor(Math.random()*3);
         setName(name[int]);
       }
       const handleClick = ()=> {
-        console.log('you clicked it');
+        setCount(count + 1)
+        setCount(count + 1)
+        console.log(count);
       }
       const handleClick2 = (name)=> {
         console.log(`${name} was clicked`);
@@ -22,9 +25,8 @@ const Content = () => {
             Hello {name}!
         </p>
         <button onClick={handlenamechange}>Change name</button>
-        <button onClick={()=>handleClick2('Karthik')}>Click It</button>
+        <button onClick={handleClick}>Click It</button>
         <button onClick={(e)=>handleClick3(e)}>Click It</button>
-
     </main>
   )
 }
