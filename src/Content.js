a2c16486a8baba8502ad84842f9afd86c3d3cf71
Karthik <getkar128@gmail.com>
2022-09-05 10:09:47 +0300

    Lists and Keys Added

--- src/Content.js
+++ src/Content.js
@@ -1,32 +1,80 @@
-import React, {useState} from "react"
+import React, {useState} from "react";
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
-    const [name, setName] = useState('Karthik')
-    const [count, setCount] = useState(0);
-    const handlenamechange = ()=> {
-        const name = ["Bob", 'Kevin', "Karthik"];
-        const int = Math.floor(Math.random()*3);
-        setName(name[int]);
+    const [items, setItems] = useState([
+      {
+        id: 1,
+        checked: false,
+        item: "One half pound a bag od Cocoa Covered Almonds Unsalted"
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Item 3"
       }
-      const handleClick = ()=> {
-        setCount(count + 1)
-        setCount(count + 1)
-        console.log(count);
-      }
-      const handleClick2 = (name)=> {
-        console.log(`${name} was clicked`);
-      }
-      const handleClick3 = (e)=> {
-        console.log(e.target.innerText);
-      }
+    ]);
+
+    const handleCheck = (id) => {
+      const listItems = items.map((item) => item.id === id? {...item, checked: !item.checked} : item);
+      setItems(listItems);
+      localStorage.setItem('ShoppingList', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+      const listItems = items.filter((item) => item.id !== id );
+      setItems(listItems)
+      localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+    // const [count, setCount] = useState(0);
+    // const handlenamechange = ()=> {
+    //     const name = ["Bob", 'Kevin', "Karthik"];
+    //     const int = Math.floor(Math.random()*3);
+    //     setName(name[int]);
+    //   }
+    //   const handleClick = ()=> {
+    //     setCount(count + 1)
+    //     setCount(count + 1)
+    //     console.log(count);
+    //   }
+    //   const handleClick2 = (name)=> {
+    //     console.log(`${name} was clicked`);
+    //   }
+    //   const handleClick3 = (e)=> {
+    //     console.log(e.target.innerText);
+    //   }
   return (
     <main>
-        <p onDoubleClick={handleClick}>
+        {/* <p onDoubleClick={handleClick}>
             Hello {name}!
         </p>
         <button onClick={handlenamechange}>Change name</button>
         <button onClick={handleClick}>Click It</button>
-        <button onClick={(e)=>handleClick3(e)}>Click It</button>
+        <button onClick={(e)=>handleClick3(e)}>Click It</button> */}
+        {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input type="checkbox"
+                onChange={()=>handleCheck(item.id)}
+                checked={item.checked} />
+              <label
+                style={(item.checked) ? {textDecoration: 'line-through'}: null}
+                onDoubleClick={()=>handleCheck(item.id)}
+              >{item.item}</label>
+              <FaTrashAlt role="button" tabIndex="0" onClick={()=> handleDelete(item.id)}/>
+            </li>
+          ))}
+        </ul>
+        ) : (
+          <p style={{marginTop: '2rem'}}>Your List is empty</p>
+        )
+        }
     </main>
   )
 }
