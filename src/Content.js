698f3a6fc5c5b09075eee6940e2069cd73b832cd
Karthik <getkar128@gmail.com>
2022-09-05 10:49:28 +0300

    Props and prop Drilling

--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,6 @@
-import React, {useState} from "react";
-import { FaTrashAlt } from 'react-icons/fa'
+import ItemList from './ItemList'
 
-const Content = () => {
-    const [items, setItems] = useState([
-      {
-        id: 1,
-        checked: false,
-        item: "One half pound a bag od Cocoa Covered Almonds Unsalted"
-      },
-      {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-      },
-      {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-      }
-    ]);
-
-    const handleCheck = (id) => {
-      const listItems = items.map((item) => item.id === id? {...item, checked: !item.checked} : item);
-      setItems(listItems);
-      localStorage.setItem('ShoppingList', JSON.stringify(listItems));
-    }
-
-    const handleDelete = (id) => {
-      const listItems = items.filter((item) => item.id !== id );
-      setItems(listItems)
-      localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
+const Content = ({items, handleCheck, handleDelete}) => {   
     // const [count, setCount] = useState(0);
     // const handlenamechange = ()=> {
     //     const name = ["Bob", 'Kevin', "Karthik"];
@@ -57,24 +27,13 @@ const Content = () => {
         <button onClick={handleClick}>Click It</button>
         <button onClick={(e)=>handleClick3(e)}>Click It</button> */}
         {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input type="checkbox"
-                onChange={()=>handleCheck(item.id)}
-                checked={item.checked} />
-              <label
-                style={(item.checked) ? {textDecoration: 'line-through'}: null}
-                onDoubleClick={()=>handleCheck(item.id)}
-              >{item.item}</label>
-              <FaTrashAlt role="button" tabIndex="0" onClick={()=> handleDelete(item.id)}/>
-            </li>
-          ))}
-        </ul>
+          <ItemList
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete} />
         ) : (
           <p style={{marginTop: '2rem'}}>Your List is empty</p>
-        )
-        }
+        )}
     </main>
   )
 }
